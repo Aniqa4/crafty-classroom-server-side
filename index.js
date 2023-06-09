31df43fc5334990ded0dc9cbce690f4040bf8f1c
Aniqa Chowdhury <aanika.chowdhury04@gmail.com>
2023-06-09 22:16:28 +0600

    retrived all classes data

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express=require('express');
-const cors=require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
-const app=express();
-const port= process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
@@ -14,50 +14,52 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const collection = client.db('Crafty-classroom').collection('users');
+        const usersCollection = client.db('Crafty-classroom').collection('users');
+        const classesCollection = client.db('Crafty-classroom').collection('classes');
 
-    //users
-    app.get("/users", async (req,res)=>{
-        const cursor= collection.find();
-        const result= await cursor.toArray();
-        res.send(result);
-    })
-    //all classes
-    app.get("/classes", async(req,res)=>{
-        const query={role:'instructor'}
-        const cursor=collection.find(query);
-        const result= await cursor.toArray();
-        res.send(result);
-    })
+        //all users
+        app.get("/users", async (req, res) => {
+            const cursor = usersCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        //all classes
+        app.get("/allclasses", async (req, res) => {
+            const cursor = classesCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('server is running...')
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is running at ${port}`);
 }) 
